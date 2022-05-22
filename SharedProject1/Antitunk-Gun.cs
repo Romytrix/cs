003e4bd2847b1ea46c331cda099f9457c76d6dd2
Romytrix <ponapiix@gmail.com>
2022-05-22 20:11:56 +0300

    CS project

---
+++ SharedProject1/Antitunk-Gun.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedProject1
+{
+    public class Antitunk_Gun
+    {
+        public string Model;
+        public bool IsWorking;
+
+        public string TypeAmmo { get; set };
+        public bool IsLoaded;
+    }
+    void Shoot
+    {
+            if(IsWorking){
+            if (IsLoaded)
+            {
+                Console.WriteLine("U shoot!");
+                IsLoaded = false;
+            }
+            else ()
+                    {
+                Console.WriteLine("U must reload!");
+            }
+        }
+            else (){
+            Console.WriteLine("Ur Tank is broken!");
+        }
+    }
+    void Reload
+    {
+            if(Isloaded){
+            Console.WriteLine("U a allready loaded!");
+        }
+            else(){
+            Console.WriteLine("Reloaded!");
+        }
+    }
+}
---
+++ SharedProject1/InfatryMan.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedProject1
+{
+    public class InfatryMan
+    {
+        public int Weight;
+        public int Height;
+        public int Endurance;
+
+        public bool IsSupply { get; set };
+
+    }
+
+    void attack()
+    {
+        if (IsSupply)
+        {
+            Console.WriteLine("He is supply, he can't attack!");
+        }
+        else (){
+            Console.WriteLine("He attacked!");
+        }
+    }
+
+    void Wait()
+    {
+        Console.WriteLine("He is waiting Ur command!");
+    }
+}
